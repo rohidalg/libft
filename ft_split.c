60a4c1e624d45be6549614105d1b0966ef4d7b2b
rohidalg <139051032+rohidalg@users.noreply.github.com>
2023-09-28 19:16:00 +0200

    28s

--- ft_split.c
+++ ft_split.c
@@ -6,16 +6,16 @@
 /*   By: rohidalg <rohidalg@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/27 18:40:41 by rohidalg          #+#    #+#             */
-/*   Updated: 2023/09/27 20:08:56 by rohidalg         ###   ########.fr       */
+/*   Updated: 2023/09/28 15:03:06 by rohidalg         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int ft_countwords(char const *s, char c)
+static int	ft_countwords(char const *s, char c)
 {
-	int count;
-	int i;
+	int	count;
+	int	i;
 
 	i = 0;
 	if (!s || !c)
@@ -23,48 +23,67 @@ static int ft_countwords(char const *s, char c)
 	count = 1;
 	while (s[i])
 	{
-		if (s[i] = c && s[i - 1] != c)
+		if (s[i] == c && s[i - 1] != c)
 			count++;
 		i++;
 	}
 	return (count);
-
 }
 
-char *ft_strnew(size_t size)
+char	*ft_strnew(size_t size)
 {
-	char *str;
+	char	*str;
 
-	str = (char *)malloc(sizeof(char*) * size + 1);
+	str = (char *)malloc(sizeof(char *) * size + 1);
 	if (!str)
 		return (0);
 	str[size] = '\0';
-	while(size--)
+	while (size--)
 		str[size] = '\0';
 	return (str);
 }
 
-char **ft_split(char const *s, char c)
+static void	ft_free(char **strmem, int x)
 {
-	unsigned int i;
-	unsigned int j;
-	char **words;
+	while (x)
+	{
+		x--;
+		free(strmem[x]);
+	}
+	free(strmem);
+}
 
-	words = (char**)mallloc(sizeof(char*) * (ft_countwords(s, c) + 1));
+char	**ft_split(char const *s, char c)
+{
+	unsigned int	i;
+	unsigned int	j;
+	char			**words;
+	unsigned int	word_start;
+	unsigned int	word_len;
+
+	words = (char **)malloc(sizeof(char *) * (ft_countwords(s, c) + 1));
 	if (!s || !words)
 		return (0);
-	while (*s != '\0')
+	i = 0;
+	j = 0;
+	while (s[i])
 	{
-		i = 0;
-		while(s[i] ! = c && s[i] != '\0')
+		while (s[i] == c)
 			i++;
-		if (i > 0)
+		if (!s[i])
+			break ;
+		word_start = i;
+		while (s[i] && s[i] != c)
+			i++;
+		word_len = i - word_start;
+		words[j] = ft_strnew(word_len);
+		if (!words[j])
 		{
-			words[j++] = ft_strncpy(ft_strnew(i), s, i);
-			s = &s[i];
+			ft_free(words, j);
+			return (0);
 		}
-		else 
-			s++;
+		ft_strlcpy(words[j], &s[word_start], word_len + 1);
+		j++;
 	}
 	words[j] = 0;
 	return (words);
