4a8b1d9a6bb9f9700257f1699acca57d158495ad
rohidalg <139051032+rohidalg@users.noreply.github.com>
2023-10-18 20:49:12 +0200

    18o1

--- ft_calloc.c
+++ ft_calloc.c
@@ -19,6 +19,8 @@ void	*ft_calloc(size_t nitems, size_t size)
 
 	i = 0;
 	tab = (unsigned char *)malloc(size * nitems);
+	if (nitems == SIZE_MAX && size == SIZE_MAX)
+		return (0);
 	if (!tab)
 		return (0);
 	while (i < nitems * size)
