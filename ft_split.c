c1f025480d15fb88ece80353f75cc8fc69d36780
Rodrigo Hidalgo Alcantara <byjocker2002@MacBook-Air-de-Rodrigo.local>
2023-11-09 10:52:57 +0100

    9n

--- ft_split.c
+++ ft_split.c
@@ -14,42 +14,42 @@
 
 int	ft_countwords(char const *str, char c)
 {
-	int i;
-	int count;
+	int	i;
+	int	count;
 
 	i = 0;
 	count = 0;
-	while(str[i])
+	while (str[i])
 	{
-		if((str[i] == c && str[i + 1] == c) || str[i + 1] == '\0')
+		if ((str[i] != c) && (str[i + 1] == c || str[i + 1] == '\0'))
 			count++;
 		i++;
 	}
 	return (count);
 }
 
-char *ft_words(char const *str, char c, int *i)
+char	*ft_words(char const *str, char c, int *i)
 {
-	char *word;
-	int start;
-	int len;
+	char	*word;
+	int		start;
+	int		len;
 
-	while(str[*i] && str[*i] == c)
+	while (str[*i] && str[*i] == c)
 		(*i)++;
 	start = *i;
-	while(str[*i] && str[*i] != c)
+	while (str[*i] && str[*i] != c)
 		(*i)++;
 	len = *i - start;
 	word = (char *)malloc(sizeof(char) * (len + 1));
-	if(!word)
+	if (!word)
 		return (0);
 	ft_strlcpy(word, str + start, len + 1);
-	return(word);
+	return (word);
 }
 
-static char **ft_free(char **string)
+static char	**ft_free(char **string)
 {
-	int i;
+	int	i;
 
 	i = 0;
 	while (string[i])
@@ -58,28 +58,28 @@ static char **ft_free(char **string)
 		i++;
 	}
 	free(string);
-	return(0);
+	return (0);
 }
 
 char	**ft_split(char const *str, char c)
 {
-	int countwords;
-	char **string;
-	int i_string;
-	int i;
+	int		countwords;
+	char	**string;
+	int		i_string;
+	int		i;
 
-	if(!str)
+	if (!str)
 		return (0);
 	countwords = ft_countwords(str, c);
 	string = ft_calloc(countwords + 1, sizeof(char *));
-	if(string == 0)
+	if (string == 0)
 		return (0);
 	i_string = 0;
 	i = 0;
-	while(i_string < countwords)
+	while (i_string < countwords)
 	{
 		string[i_string] = ft_words(str, c, &i);
-		if(!string[i_string])
+		if (!string[i_string])
 			return (ft_free(string));
 		i_string++;
 	}
